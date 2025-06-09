1f381759e634fe113b33832183d14e6fc1648de3
Orca <stephanie.hu2@student.tdsb.on.ca>
2025-06-09 18:04:17 -0400

    Update App.java

--- e-master/src/main/java/summative/App.java
+++ e-master/src/main/java/summative/App.java
@@ -1,10 +1,27 @@
-module summative {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.swing;
-    requires transitive javafx.graphics;
+package summative;
 
-    opens summative to javafx.fxml;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    exports summative;
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("primary.fxml"));
+        Parent root = loader.load();
+
+        PrimaryController controller = loader.getController();
+        controller.setStage(primaryStage);
+
+        primaryStage.setTitle("Summative");
+        primaryStage.setScene(new Scene(root, 750, 750));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
